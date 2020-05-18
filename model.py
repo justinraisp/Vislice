b2873f3584a8bbd887489c226093768d9784da3a
Justin Raišp <justin.raisp37@gmail.com>
2020-05-18 19:36:02 +0200

    Dodali bottle

--- model.py
+++ model.py
@@ -2,6 +2,8 @@ import random
 
 STEVILO_DOVOLJENIH_NAPAK = 10
 
+ZACETEK = 'Z'
+
 # Konstante za rezultate ugibanj
 PRAVILNA_CRKA = '+'
 PONOVLJENA_CRKA = 'o'
@@ -90,3 +92,43 @@ def nova_igra():
     return Igra(nakljucna_beseda)
 
 
+class Vislice:
+    '''
+    Skrbi za trenutno stanje VEČ iger (imel bo več objektov tipa Igra)
+    '''
+    def __init__(self):
+        # Slovar, ki ID-ju priredi objekt njegove igre
+        self.igre = {}       #  int -> igra
+
+
+    def prosti_id_igre(self):
+        '''Vrne nek id, ki ga ne uporablja nobena igra'''
+
+        if len(self.igre) == 0:
+            return 0
+        else:
+            return max(self.igre.keys()) + 1
+
+    def nova_igra(self):
+
+        # dobimo svež id
+        nov_id = self.prosti_id_igre()
+
+        # naredimo novo igro
+        sveza_igra = nova_igra()
+
+        # vse to shranimo v self.igre
+        self.igre[nov_id] = (sveza_igra, ZACETEK)
+
+        # Vrnemo id
+        return nov_id
+
+    def ugibaj(self, id_igre, crka):
+        # Dobimo staro igro ven
+        trenutna_igra, _ = self.igre[id_igre]
+
+        # Ugibamo črko
+        novo_stanje = trenutna_igra.ugibaj(crka)
+
+        # Zapišemo posodbljeno stanje in igro nazaj v 'BAZO'
+        self.igre[id_igre] = (trenutna_igra, novo_stanje)
